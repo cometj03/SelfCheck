5bc3ac7159c6a45eb408d966e035497b7dd3deb8
XxCtrlZxX <harryjung1001@gmail.com>
2021-03-18 15:17:40 +0900

    Add Data.py

---
+++ Data.py
@@ -0,0 +1,11 @@
+class Data:
+    def __init__(self):
+        self.school = ""
+
+    def get(self):
+        json_data = {}
+        self.school_level = json_data['school_level']
+        self.school_name = json_data['school_name']
+        self.name = json_data['name']
+        self.birth = json_data['birth']
+        self.password = json_data['password']
